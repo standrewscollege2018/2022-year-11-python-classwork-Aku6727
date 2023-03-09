e5197486caa8975599537b88c8388392f0496ff4
Aku6727 <antkutovoy@gmail.com>
2023-03-09 19:57:24 +1300

    Tested and finished the Rental cars task

--- Rental Cars task.py
+++ Rental Cars task.py
@@ -1,40 +1,95 @@
-''' Rental Cars Task '''
+""" Rental Cars Task """
 
 # Cars and details
-cars = ["Suzuki Van", "Toyota Corolla", "Honda CRV", "Suzuki Swift", "Mitsubishi Airtek", "Nissan DC Ute", "Tayota Previa", "Toyota Hi Ace", "Toyota Hi Ace"]
+cars = [
+    "Suzuki Van",
+    "Toyota Corolla",
+    "Honda CRV",
+    "Suzuki Swift",
+    "Mitsubishi Airtek",
+    "Nissan DC Ute",
+    "Tayota Previa",
+    "Toyota Hi Ace",
+    "Toyota Hi Ace",
+]
 seat_number = ["2", "4", "4", "4", "4", "4", "7", "12", "12"]
-status_of_availiability = ["Availiable", "Availiable", "Availiable", "Availiable", "Availiable", "Availiable", "Availiable", "Availiable", "Availiable"]
+status_of_availiability = [
+    "Availiable",
+    "Availiable",
+    "Availiable",
+    "Availiable",
+    "Availiable",
+    "Availiable",
+    "Availiable",
+    "Availiable",
+    "Availiable",
+]
 
 keep_working = True
 while keep_working == True:
 
+    # Display th list of cars
     for i in range(len(cars)):
-        print(f"{i+1:<5}. {cars[i]:<20}  Seats in the car: {seat_number[i]:<5}  {status_of_availiability[i]:<20}")
+        print(
+            f"{i+1:<5}. {cars[i]:<20}  Seats in the car: {seat_number[i]:<5}  {status_of_availiability[i]:<20}"
+        )
 
-    keep_asking = True
-    while keep_asking == True:
+
+    # Hire the car
+    ask_for_a_car = True
+    while ask_for_a_car == True:
         try:
-            car_rent_name = input("What is your name (type EXIT to exit):  ")
-
-
-
+            car_select = int(
+                input(
+                    "Please enter the number of the car you want to hire (0 to exit):  "
+                )
+            )
+            if car_select == 0:
+                break
+            elif car_select > len(cars) or car_select < 0:
+                print("That is not valid")
+            else:
+                print("Thank you")
+                ask_for_a_car = False
         except ValueError:
-            print(f"{car_rent_name} is not valid. Please try again")
-
-        if car_rent_name == "EXIT":
-            for i in range(len(cars)):
-                print(f"{i+1:<5}. {cars[i]:<20}  Seats in the car: {seat_number[i]:<5}  {status_of_availiability[i]:<20}")
-                print("Ok, please restart to continue")
-        else:
-            print(f"Would you like to rent a car {car_rent_name}")
-            input("Yes/No:  ")
+            print("Enter an integer")
 
 
+    if car_select != 0:
+        # Get the last name and check it
+        check_last_name = True
+        while check_last_name == True:
+            try:
+                car_rent_last_name = input("What is your last name: ")
+
+                if car_rent_last_name == "0":
+                    break
+                elif car_rent_last_name.isalpha():
+                    check_last_name = False
+                    # Change the car availiability
+                    if status_of_availiability[car_select - 1] == "Unavailiable":
+                        print("You cannot rent this car")
+                    else:
+                        status_of_availiability[car_select - 1] = "Unavailiable"
+                        print("Ok")
+                        print(f"You have selected {cars[car_select-1]}")
+
+                    ask_for_a_car = False
+                else:
+                    print("Error")
 
 
+            except:
+                print("Error")
 
 
-
+    # User has quit
+    else:
+        keep_working = False
+        for i in range(len(cars)):
+            print(
+                f"{i+1:<5}. {cars[i]:<20}  Seats in the car: {seat_number[i]:<5}  {status_of_availiability[i]:<20}"
+            )
 
 
 
--- Test Page.py
+++ Test Page.py
@@ -1,1 +1,82 @@
+""" Rental Cars Task """
 
+# Cars and details
+cars = [
+    "Suzuki Van",
+    "Toyota Corolla",
+    "Honda CRV",
+    "Suzuki Swift",
+    "Mitsubishi Airtek",
+    "Nissan DC Ute",
+    "Tayota Previa",
+    "Toyota Hi Ace",
+    "Toyota Hi Ace",
+]
+seat_number = ["2", "4", "4", "4", "4", "4", "7", "12", "12"]
+status_of_availiability = [
+    "Availiable",
+    "Availiable",
+    "Availiable",
+    "Availiable",
+    "Availiable",
+    "Availiable",
+    "Availiable",
+    "Availiable",
+    "Availiable",
+]
+
+keep_working = True
+while keep_working == True:
+
+    # Display th list of cars
+    for i in range(len(cars)):
+        print(
+            f"{i+1:<5}. {cars[i]:<20}  Seats in the car: {seat_number[i]:<5}  {status_of_availiability[i]:<20}"
+        )
+    # Hire the car
+
+    ask_for_a_car = True
+    while ask_for_a_car == True:
+        try:
+            car_select = int(
+                input(
+                    "Please enter the corresponding number of the car you want to hire (0 to exit):  "
+                )
+            )
+            if car_select == 0:
+                break
+                ask_for_a_car == False
+            elif car_select > len(cars) or car_select < 0:
+                print("That is not valid")
+            else:
+                # Change the car availiability
+                if status_of_availiability[car_select - 1] == "Unavailiable":
+                    print("You cannot rent this car")
+                else:
+                    status_of_availiability[car_select - 1] = "Unavailiable"
+                    print("Ok")
+                    print(f"You have selected {cars[car_select-1]}")
+
+                    ask_for_a_car = False
+        except ValueError:
+            print("Enter an integer")
+    if car_select != 0:
+        # Get the last name and check it
+        check_last_name = True
+        while check_last_name == True:
+            car_rent_last_name = input("What is your last name: ")
+
+            if car_rent_last_name.isalpha():
+                check_last_name = False
+
+            elif car_rent_last_name.strip(" ") == "":
+                print("Can't be blank")
+            else:
+                print("Error, enter a valid name")
+    # User has quit
+    else:
+        keep_working = False
+        for i in range(len(cars)):
+            print(
+                f"{i+1:<5}. {cars[i]:<20}  Seats in the car: {seat_number[i]:<5}  {status_of_availiability[i]:<20}"
+            )
