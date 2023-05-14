62c79d5584991c0c68ad158b1916c94ce6bbadea
Aku6727 <antkutovoy@gmail.com>
2023-05-15 09:43:48 +1200

    Random work
    I fixed some issues with different codes

--- Fibonacci Sequence task.py
+++ Fibonacci Sequence task.py
@@ -1,1 +1,11 @@
+n_1 = "1"
+n_2 = "2"
+print(f"{n_1}")
+print(f"{n_2}")
+keep_running = True
+while keep_running == True:
+    n_3 = n_1 + n_2
+    print(f"{n_3}")
+    n_1=n_2
+    n_2=n_3
 
--- Titanic passanger search task.py
+++ Titanic passanger search task.py
@@ -1,19 +1,24 @@
-'''Search for data on titanic passangers'''
-#import the library
+"""Search for data on titanic passangers"""
+# import the library
 import sqlite3
-#set a name for the database
-DATABASE = 'Titanic_search_passanger.db'
-#make a connection
+
+# set a name for the database
+DATABASE = "Titanic_search_passanger.db"
+# make a connection
 connection = sqlite3.connect(DATABASE)
 cursor = connection.cursor()
 # start the loop
 keep_running = True
 while keep_running == True:
-    #ask what the user wants to do
+    # ask what the user wants to do
     ask_at_menu = True
     while ask_at_menu == True:
         try:
-            menu_choice = int(input(" Welcome to the titanic database \n================================\n Menu \nOptions: \n 1. Search by name \n 2. Search by Class \n 3. Exit \n "))
+            menu_choice = int(
+                input(
+                    " Welcome to the titanic database \n================================\nMenu \nOptions: \n 1. Search by name \n 2. Search by Class \n 3. Exit \n "
+                )
+            )
             if menu_choice == 1 or menu_choice == 2 or menu_choice == 3:
                 ask_at_menu = False
                 print("Ok")
@@ -22,124 +27,71 @@ while keep_running == True:
         except ValueError:
             print("Please enter a number")
 
-    #Execute option 1
+    # Execute option 1
     if menu_choice == 1:
         name_validity = True
         while name_validity == True:
             name = input("What is the name of the person? ")
-            if name.isalpha():
-                #Execute the querie
-                name = "%" + name + "%"
-                cursor.execute("SELECT passenger_id, name, class, survived FROM titanic where name Like ?",(name,))
-                name_results = cursor.fetchall()
-                #Print the results
-                print(f"Results: \n======== \n{'ID':<4} {'Name':<70} {'Class':<10} {'Survival (1 for alive, 0 for dead)':<15}")
-                for name_result in name_results:
-                    print(f"{result[0]:4} {result[1]:70} {result[2]:<10} {result[3]:<15}")
-                print("=========================================\n \n")
-                name_validity = False
-
-            else:
-                print("Enter a valid name or part of a name")
-    #Execute option 2
-    elif menu_choice == 2:
-        #Get and validate the class
-        class_validity = True
-        while class_validity == True:
-            passenger_class=input("Enter a class (1-3 or 0 to quit): ")
-            if passenger_class == 0:
-                class_validity = False
-                keep_running = False
-            elif passenger_class.isnumeric():
-                #Get the results
-                cursor.execute("SELECT passenger_id, name, class, survived FROM Titanic where class = ?",(passenger_class,))
-                class_results = cursor.fetchall()
-                #Print the results
-                print(f"Results: \n======== \n{'ID':<4} {'Name':<70} {'Class':<10} {'Survival (1 for alive, 0 for dead)':<15}")
-                for class_result in class_results:
-                    print(f"{class_result[0]:4} {class_result[1]:70} {class_result[2]:<10} {class_result[3]:<15}")
-                class_validity = False
-                print("=======================================\n \n")
-            else:
-                print("Enter a valid class")
-
-    #Execute option 3 and exit
-    elif menu_choice == 3:
-        keep_running = False
-    else:
-        print("Please enter a valid action ")
-
-'''Search for data on titanic passangers'''
-#import the library
-import sqlite3
-#set a name for the database
-DATABASE = 'Titanic_search_passanger.db'
-#make a connection
-connection = sqlite3.connect(DATABASE)
-cursor = connection.cursor()
-# start the loop
-keep_running = True
-while keep_running == True:
-    #ask what the user wants to do
-    ask_at_menu = True
-    while ask_at_menu == True:
-        try:
-            menu_choice = int(input(" Welcome to the titanic database \n================================\n Menu \nOptions: \n 1. Search by name \n 2. Search by Class \n 3. Exit \n "))
-            if menu_choice == 1 or menu_choice == 2 or menu_choice == 3:
-                ask_at_menu = False
-                print("Ok")
-            else:
-                print("Please enter a valid action")
-        except ValueError:
-            print("Please enter a number")
-
-    #Execute option 1
-    if menu_choice == 1:
-        name_validity = True
-        while name_validity == True:
-            name = input("What is the name of the person? ")
-
             if name.isnumeric():
-                print("Must enter some text")
-            elif name.strip(" ") == "":
-                print("Must enter something")
+                print("Please enter a valid title")
+            elif name.replace(" ", "") == "":
+                print(f"---{name}--- is blank")
+
             else:
-                #Execute the querie
+                # Execute the querie
                 name = "%" + name + "%"
-                cursor.execute("SELECT passenger_id, name, class, survived FROM titanic where name Like ?",(name,))
+                cursor.execute(
+                    "SELECT passenger_id, name, class, survived FROM titanic where name Like ?",
+                    (name,),
+                )
                 name_results = cursor.fetchall()
-                #Print the results
-                print(f"Results: \n======== \n{'ID':<4} {'Name':<70} {'Class':<10} {'Survival (1 for alive, 0 for dead)':<15}")
+                # Print the results
+
+                if name_results[3] == 0:
+                    name_results[3] = "Dead"
+                else:
+                    name_results[3] = "Alive"
+                print(
+                    f"Results: \n======== \n{'ID':<4} {'Name':<70} {'Class':<10} {'Survival (1 for alive, 0 for dead)':<15}"
+                )
                 for name_result in name_results:
-                    print(f"{result[0]:4} {result[1]:70} {result[2]:<10} {result[3]:<15}")
+                    print(
+                        f"{name_result[0]:4} {name_result[1]:70} {name_result[2]:<10} {name_result[3]:<15}"
+                    )
                 print("=========================================\n \n")
                 name_validity = False
 
-    #Execute option 2
+    # Execute option 2
     elif menu_choice == 2:
-        #Get and validate the class
+        # Get and validate the class
         class_validity = True
         while class_validity == True:
-            passenger_class=input("Enter a class (1-3 or 0 to quit): ")
+            passenger_class = input("Enter a class (1-3 or 0 to quit): ")
             if passenger_class == 0:
                 class_validity = False
                 keep_running = False
             elif passenger_class.isnumeric():
-                #Get the results
-                cursor.execute("SELECT passenger_id, name, class, survived FROM titanic where class = ?",(passenger_class,))
+                # Get the results
+                cursor.execute(
+                    "SELECT passenger_id, name, class, survived FROM Titanic where class = ?",
+                    (passenger_class,),
+                )
                 class_results = cursor.fetchall()
-                #Print the results
-                print(f"Results: \n======== \n{'ID':<4} {'Name':<70} {'Class':<10} {'Survival (1 for alive, 0 for dead)':<15}")
+                # Print the results
+                print(
+                    f"Results: \n======== \n{'ID':<4} {'Name':<70} {'Class':<10} {'Survival (1 for alive, 0 for dead)':<15}"
+                )
                 for class_result in class_results:
-                    print(f"{class_result[0]:4} {class_result[1]:70} {class_result[2]:<10} {class_result[3]:<15}")
+                    print(
+                        f"{class_result[0]:4} {class_result[1]:70} {class_result[2]:<10} {class_result[3]:<15}"
+                    )
                 class_validity = False
                 print("=======================================\n \n")
             else:
                 print("Enter a valid class")
 
-    #Execute option 3 and exit
+    # Execute option 3 and exit
     elif menu_choice == 3:
         keep_running = False
     else:
         print("Please enter a valid action ")
-
